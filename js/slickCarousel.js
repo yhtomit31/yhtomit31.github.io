d2eedd871903c0760a592a36c84e2ed77ff27d95
tim <thein@bedford.com>
2020-08-26 10:07:49 -0500

    slick controls click events

--- js/slickCarousel.js
+++ js/slickCarousel.js
@@ -8,11 +8,20 @@ $(function () {
 })
 
 slider.slick({
-  appendArrows: $(".slick-list"),
   autoplay: true,
   autoplaySpeed: 5000,
+  arrows: false,
+  pauseOnFocus: false,
+  pauseOnHover: false,
   slidesToShow: 1,
   slidesToScroll: 1,
   speed: 300,
-  centerMode: true,
 });
+
+$(".fas").on("click", (event) => {
+  let className = event.target.className;
+  className.includes("left") ?
+      slider.slick("slickPrev")
+    :
+      slider.slick("slickNext")
+})
