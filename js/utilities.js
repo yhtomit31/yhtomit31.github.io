8c4626c51181d777e35b2e7d40efc1c74864130c
tim <thein@bedford.com>
2020-09-10 10:23:48 -0500

    add switch to utilities show right img addToCart

--- js/utilities.js
+++ js/utilities.js
@@ -1,27 +1,68 @@
-function notifyUserAddedToCart() {
-    // declare and assign element and check if on DOM ? remove() : ""
-    var oldAddToCart =  document.getElementsByClassName("add-to-cart-container")[0];
-    if (oldAddToCart) { oldAddToCart.remove(); }
-    
-    // declare and assign variables for new div creation
-    var newDiv = document.createElement("div");
-    var itemCountSpan = document.createElement("span");
-    var cartIcon = document.createElement("i");
+function notifyUserAddedToCart(value, item) {
+  // declare and assign element and check if on DOM ? remove() : ""
+  var oldAddToCart = document.getElementsByClassName(
+    "add-to-cart-container"
+  )[0];
+  if (oldAddToCart) {
+    oldAddToCart.remove();
+  }
 
-    // add class name to created div and style
-    newDiv.classList.add("add-to-cart-container");
-    newDiv.style.position = "fixed";
-    newDiv.style.top = "4em";
-    newDiv.style.right = "4em";
+  // declare and assign variables for new div creation
+  var newDiv = document.createElement("div");
+  var itemCountSpan = document.createElement("span");
 
-    // assign value from localStorage to shopping cart
-    itemCountSpan.innerHTML = localStorage.getItem("cartItems");
+  // add class name to created div
+  newDiv.classList.add("add-to-cart-container", "added-to-cart");
 
-    // add className to newDiv child element
-    cartIcon.className = "fas fa-shopping-cart added-to-cart";
+  // assign value from localStorage to shopping cart
+  itemCountSpan.innerHTML = localStorage.getItem("cartItems");
 
-    //  append new elements to DOM
-    cartIcon.appendChild(itemCountSpan);
-    newDiv.appendChild(cartIcon);    
-    document.body.appendChild(newDiv, cartIcon)
+  ////// Module 3
+  // create img element and set attributes, insert item count next to img, and add to DOM
+  var img = document.createElement("img");
+
+  // Condition switch statement to adjust DOM based on "item"  value pased into function
+  switch (item) {
+    // assign macaron src attribute of img, insert adjacent item count element, and add to dom
+    case "macaron(s)": {
+      img.src = "images/macaron.png";
+      newDiv.appendChild(img);
+      img.insertAdjacentElement("afterend", itemCountSpan);
+      document.body.appendChild(newDiv, img);
+      break;
+    }
+    // assign cake src attribute of img, insert adjacent item count element, and add to dom
+    case "cake(s)": {
+      img.src = "images/food-and-restaurant.png";
+      newDiv.appendChild(img);
+      img.insertAdjacentElement("afterend", itemCountSpan);
+      document.body.appendChild(newDiv, img);
+      break;
+    }
+    // assign cupcake src attribute of img, insert adjacent item count element, and add to dom
+    case "cupcake(s)": {
+      img.src = "images/muffin.png";
+      newDiv.appendChild(img);
+      img.insertAdjacentElement("afterend", itemCountSpan);
+      document.body.appendChild(newDiv, img);
+      break;
+    }
+    // assign src attribute of img, insert adjacent item count element, and add to dom
+    case "cookie(s)": {
+      img.src = "images/mould.png";
+      newDiv.appendChild(img);
+      img.insertAdjacentElement("afterend", itemCountSpan);
+      document.body.appendChild(newDiv, img);
+      break;
+    }
+    // default behavior if no case conditions are met.
+    default: {
+      var cartIcon = document.createElement("i");
+      cartIcon.className = "fas fa-shopping-cart added-to-cart";
+      cartIcon.appendChild(itemCountSpan);
+      newDiv.appendChild(cartIcon);
+      document.body.appendChild(newDiv, cartIcon);
+      break;
+    }
+  }
 }
